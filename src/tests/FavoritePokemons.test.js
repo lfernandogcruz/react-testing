ec80290e6da6d05aa9873dd4f951ccca85484003
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-05-12 02:59:03 -0300

    req 03 - teste o comp FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,8 +1,78 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+// import FavoritePokemons from '../components/FavoritePokemons';
 
-// 3. Teste o componente <FavoritePokemons.js />
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  return {
+    ...render(<Router history={ history }>{component}</Router>),
+    history,
+  };
+};
 
-// Teste se é exibida na tela a mensagem 'No favorite pokemon found',
-// caso a pessoa não tenha pokémons favoritos.
+const goToFavPage = () => {
+  const link2fav = screen.getByRole('link', { name: /favorite pokémons/i });
+  expect(link2fav).toBeInTheDocument();
 
-// Teste se são exibidos todos os cards de pokémons favoritados.
+  userEvent.click(link2fav);
+  // expect(history.location.pathname).toBe('/favorites');
+
+  const favHeader = screen.getByRole('heading', {
+    name: /Favorite pokémons/i,
+    level: 2,
+  });
+  expect(favHeader).toBeInTheDocument();
+};
+
+describe('3. Teste o componente <FavoritePokemons.js />', () => {
+  // 3. Teste o componente <FavoritePokemons.js />
+  test('Teste se é exibida na tela a mensagem "No favorite pokemon found"', () => {
+    // Teste se é exibida na tela a mensagem 'No favorite pokemon found',
+    // caso a pessoa não tenha pokémons favoritos.
+    renderWithRouter(<App />);
+
+    goToFavPage();
+
+    const noFavPknTxt = 'No favorite pokemon found';
+    const fav404 = screen.getByText(noFavPknTxt);
+    expect(fav404).toBeInTheDocument();
+  });
+
+  test('Teste se são exibidos todos os cards de pokémons favoritados.', () => {
+    // Teste se são exibidos todos os cards de pokémons favoritados.
+    renderWithRouter(<App />);
+
+    const go2DetailsAndFavPkmnAndBackHome = () => {
+      const pkmnDetailsLink = screen.getByRole('link', { name: /More details/i });
+      expect(pkmnDetailsLink).toBeInTheDocument();
+      userEvent.click(pkmnDetailsLink);
+
+      const favCheckbox = screen.getByRole('checkbox', {
+        name: /pokémon favoritado\?/i,
+      });
+      expect(favCheckbox).toBeInTheDocument();
+      userEvent.click(favCheckbox);
+
+      const homeLink = screen.getByRole('link', { name: /home/i });
+      expect(homeLink).toBeInTheDocument();
+      userEvent.click(homeLink);
+    };
+
+    go2DetailsAndFavPkmnAndBackHome();
+
+    const nextPkmnBtn = screen.getByRole('button', { name: /próximo pokémon/i });
+    expect(nextPkmnBtn).toBeInTheDocument();
+    userEvent.click(nextPkmnBtn);
+
+    go2DetailsAndFavPkmnAndBackHome();
+    goToFavPage();
+
+    const favCards = screen.getAllByRole('img', { name: /is marked as favorite/i });
+    expect(favCards.length).toBe(2);
+    // screen.logTestingPlaygroundURL();
+  });
+});
